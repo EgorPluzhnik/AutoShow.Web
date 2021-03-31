0f56d58fd6d9d2f1a5a671bfc0e2d60b6ed3baeb
VladSavitskiy <cyberl1fe@yandex.ru>
2021-04-01 00:18:49 +0500

    Remove unused files

--- src/main/java/com/web/autoshow/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.autoshow.repositories;
-
-import com.web.autoshow.models.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
