5acab82c7f3c0b1f9678ebd6cad8b2f00f3a86b5
danil.sechin <reymonsma@gmail.com>
2021-04-01 20:16:45 +0500

    A new func to get pid

--- src/main/java/com/web/autoshow/dao/AuthDAO.java
+++ src/main/java/com/web/autoshow/dao/AuthDAO.java
@@ -1,7 +1,11 @@
 package com.web.autoshow.dao;
 
+import com.web.autoshow.models.Auth;
 import com.web.autoshow.repositories.AuthRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AuthDAO {
@@ -13,5 +17,17 @@ public class AuthDAO {
 
     public boolean exists(long pid) {
         return authRepository.existsById(pid);
+    }
+
+    public long getPid(String login, String password) {
+        List<Auth> auths = new ArrayList<>();
+        authRepository.findAll().forEach(auths::add);
+        for (Auth auth : auths) {
+            if (auth.getLogin().equals(login) && auth.getPassword().equals(password)) {
+                return auth.getPid();
+            }
+        }
+
+        return -1;
     }
 }
